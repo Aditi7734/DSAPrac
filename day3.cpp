80628905e572030160992e1698351151df8f07b4
Aditi Supriya <148896567+Aditi7734@users.noreply.github.com>
2025-12-12 11:27:37 +0530

    Add array reversal function and main logic
    Implement a function to reverse an array in place and update main to handle input and output correctly.

--- day3.cpp
+++ day3.cpp
@@ -1,1 +1,30 @@
+/* Problem Statement:
+Given an array of integers, write a function to reverse the array in place (without using extra space for another array).
+Example:
+- Input: [1, 2, 3, 4, 5]
+- Output: [5, 4, 3, 2, 1] */
+#include<iostream>
+using namespace std;
+void reverse(int n,int arr[n]){
 
+
+  
+}
+int main(){
+int n;
+cout<<"numbers you have:";
+cin>>n;
+cout<<"enter the number please: ";
+int arr[n];
+for(int i=0;i<n;i++){
+  cin<<arr[i];
+}
+cout<<"Array in Reversing order: ";
+for(int i=n;i>0;i++){
+  cout<<arr[i];
+}
+
+
+
+  
+}

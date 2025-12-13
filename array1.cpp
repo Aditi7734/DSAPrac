cb4a4b79e69a9086a2030ee070a4cdd4121ed9ca
Aditi Supriya <148896567+Aditi7734@users.noreply.github.com>
2025-12-13 10:55:47 +0530

    Correct array initialization and output syntax
    Fix syntax errors in array initialization and output.

--- array1.cpp
+++ array1.cpp
@@ -5,7 +5,9 @@ int arr[10] = {1,2,3,4,5,6}
 char crr[5] = {a,e,i,o,u}
 string prr[5] = {"Hello","fun","nun","sun","tone"}
 for(int i=0;i<n;i++){
-  cout<<arr[i]
+  cout<<arr[i];
+  cout<<crr[i];
+  cout<<prr[i];
 }
   
 
